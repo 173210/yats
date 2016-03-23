c82a6396ff37a8dca92894f7bc41a99c35634d62
173210 <root.3.173210@live.com>
2016-03-23 10:33:06 +0900

    Remove redundant functions

--- import.js
+++ import.js
@@ -13,9 +13,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
-window.onerror = function(message) {
-	alert(message);
-}
+window.onerror = alert;
 
 const open = window.indexedDB.open("tweets", 4);
 var openDone = false;
